5cc5b95a9459cc68d8cd0f0e67ecd069836ad42c
Julia-Fumis <jujufumis99@gmail.com>
2023-11-21 21:57:10 -0300

    Corrijindo calculo do nivel do heroi e adicionando funcao para calculo do saldo de (vitorias - derrotas)

--- Heroi.js
+++ Heroi.js
@@ -1,6 +1,6 @@
 let nome = prompt("Digite o nome do Heroi: ")
 let xp = prompt("Digite a experiencia do seu Heroi: ")
-let lvl
+let lvl, lvlRank
 
 if (xp <= 1000) {
     lvl = "Ferro"
@@ -19,4 +19,31 @@ if (xp <= 1000) {
 } else {
     lvl= "Radiante"
 }
-    console.log (` O herói de nome ${nome} está no nível ${nível}`)
+    console.log (` O herói de nome ${nome} está no nível ${lvl}`)
+
+let vitorias = prompt("Digite a quantidade de vitorias")
+let derrotas = prompt("Digite a quantidade de derrotas")
+
+const saldoRankeadas = (vitorias, derrotas) => {
+    return vitorias - derrotas
+}
+
+let saldoRanks = saldoRankeadas(vitorias, derrotas)
+
+if (saldoRanks <= 10) {
+    lvlRank = "Ferro"
+} else if (saldoRanks <= 20) {
+    lvlRank = "Bronze"
+} else if (saldoRanks <= 50) {
+    lvlRank = "Prata"
+} else if (saldoRanks <= 80) {
+    lvlRank = "Ouro"
+} else if (saldoRanks <= 90) {
+    lvlRank = "Diamante"
+} else if (saldoRanks <= 100) {
+    lvlRank = "Lendario"
+} else  {
+    lvlRank = "Imortal"
+} 
+
+console.log(`O Heroi tem de saldo ${saldoRanks} esta no nivel de ${lvlRank}`)
